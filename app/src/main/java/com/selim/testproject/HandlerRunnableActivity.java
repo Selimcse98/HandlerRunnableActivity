f6da03b6216e7c50d10c4872189480729a05a71e
Mohammad Selim Miah <selimcse98@gmail.com>
2016-06-13 17:05:13 +1000

    first commit

---
+++ app/src/main/java/com/selim/testproject/HandlerRunnableActivity.java
@@ -0,0 +1,109 @@
+package com.selim.testproject;
+
+/**
+ * Created by mohammadselimmiah on 13/06/2016.
+ */
+import android.app.Activity;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.ProgressBar;
+import android.widget.Toast;
+
+public class HandlerRunnableActivity extends Activity {
+
+    private ImageView mImageView;
+    private ProgressBar mProgressBar;
+    private Bitmap mBitmap;
+    private int mDelay = 1000;
+    private final Handler handler = new Handler();
+    private boolean isImageLoaded =false;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mImageView = (ImageView) findViewById(R.id.imageView);
+        mProgressBar = (ProgressBar) findViewById(R.id.progressBar);
+
+        final Button loadButton = (Button) findViewById(R.id.loadButton);
+        loadButton.setOnClickListener(new OnClickListener() {
+            public void onClick(View v) {
+                new Thread(new LoadIconTask(R.drawable.painter)).start();
+            }
+        });
+
+        final Button otherButton = (Button) findViewById(R.id.otherButton);
+        otherButton.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(HandlerRunnableActivity.this, isImageLoaded?"Image Loaded Successfully":"I'm Working ...", Toast.LENGTH_LONG).show();
+                //Toast.makeText(HandlerRunnableActivity.this,"I am Working ...", Toast.LENGTH_LONG).show();
+            }
+        });
+
+    }
+
+    private class LoadIconTask implements Runnable {
+        int resId;
+
+        LoadIconTask(int resId) {
+            this.resId = resId;
+        }
+
+        public void run() {
+
+            handler.post(new Runnable() {
+                @Override
+                public void run() {
+                    mProgressBar.setVisibility(ProgressBar.VISIBLE);
+                }
+            });
+
+            mBitmap = BitmapFactory.decodeResource(getResources(), resId);
+
+            // Simulating long-running operation
+
+            for (int i = 1; i < 11; i++) {
+                sleep();
+                final int step = i; //Final variable can be assigned once for each declaration
+                handler.post(new Runnable() {
+                    @Override
+                    public void run() {
+                        mProgressBar.setProgress(step * 10);
+                    }
+                });
+            }
+
+            handler.post(new Runnable() {
+                @Override
+                public void run() {
+                    mImageView.setImageBitmap(mBitmap);
+                }
+            });
+
+            handler.post(new Runnable() {
+                @Override
+                public void run() {
+                    isImageLoaded = true;
+                    mProgressBar.setVisibility(ProgressBar.INVISIBLE);
+                }
+            });
+        }
+    }
+
+    private void sleep() {
+        try {
+            Thread.sleep(mDelay);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+}
